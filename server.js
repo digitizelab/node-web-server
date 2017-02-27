3417ae634ace2fff569a6e5000c78baa8ba1c045
Nishan Karunarathna <Nishan Karunarathna>
2017-02-27 08:20:29 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,63 @@
+const express = require('express');
+//Handlebars 
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+//Register partials
+hbs.registerPartials(__dirname + '/views/partials');
+//Set the view engine to Handlebars
+app.set('view engine', 'hbs');
+
+//Request middleware
+app.use((req, res, next) => {
+	var now = new Date().toString();
+	var log = `${now}: ${req.method} ${req.url}`;
+
+	console.log(log);
+	fs.appendFile('server.log', log + '\n', (err) => {
+		if (err) {
+			console.log('Unable to append to server.log');
+		}
+	});
+	next();
+});
+
+// app.use((req, res, next) => {
+// 	res.render('maintenance.hbs');
+// });
+
+//Using middleware serve static pages
+app.use(express.static(__dirname + '/public'));
+
+//Register view helpers
+hbs.registerHelper('getCurrentYear', () => {
+	return new Date().getFullYear()
+});
+hbs.registerHelper('screamIt', (text) => {
+	return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+	res.render('home.hbs', {
+		pageTitle: 'Home Page',
+		welcomeMessage: 'Welcome to Homepage'
+	});
+});
+
+app.get('/about', (req, res) => {
+	res.render('about.hbs', {
+		pageTitle: 'About Page'
+	});
+});
+
+app.get('/bad', (req, res) => {
+	res.send({
+		error: 'Unable to process this request'
+	});
+});
+
+app.listen(3000, () => {
+	console.log('Server is up on port 3000');
+});
